3c8132719de82694a20afb1a036d24b5d27ff023
benni.huff@gmail.com <benni.huff@gmail.com>
2026-05-19 16:40:43 +0200

    stuff for saving but doesnt work i think im to dumb D:

--- server/api/country/.post.ts
+++ server/api/country/.post.ts
@@ -2,12 +2,17 @@ import { Countries } from "#server/storage/country";
 import { Country } from "#shared/types/types";
 
 export default defineEventHandler(async (event) => {
-	const newCountry = await readValidatedBody<Country>(event, (data) => {
-		return Country.parse(data);
-	});
+  const newCountry = await readValidatedBody<Country>(event, (data) => {
+    console.log(data);
 
-	const country = await Countries.create(newCountry);
-	if (country instanceof Error) throw country;
+    return Country.parse(data);
+  });
 
-	return country;
+  console.log(2);
+  console.log(newCountry);
+
+  const country = await Countries.create(newCountry);
+  if (country instanceof Error) throw country;
+
+  return country;
 });
