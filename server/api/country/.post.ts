7a89cabf9a1d525fff01935aca838e44f18a53a9
benni.huff@gmail.com <benni.huff@gmail.com>
2026-05-22 09:46:01 +0200

    fixed api endpoint

--- server/api/country/.post.ts
+++ server/api/country/.post.ts
@@ -2,14 +2,12 @@ import { Countries } from "#server/storage/country";
 import { type Country, countrySchema } from "#shared/types/types";
 
 export default defineEventHandler(async (event) => {
-	const newCountry = await readValidatedBody<Country>(event, (data) => {
-		return countrySchema.parse(data);
-	});
+  const newCountry = await readValidatedBody<Country>(event, (data) => {
+    return countrySchema.parse(data);
+  });
 
-	console.log(newCountry);
+  const country = await Countries.create(newCountry);
+  if (country instanceof Error) throw country;
 
-	const country = await Countries.create(newCountry);
-	if (country instanceof Error) throw country;
-
-	return country;
+  return country;
 });
